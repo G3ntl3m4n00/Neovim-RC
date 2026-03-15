91cbe341aed1b0ef59aa8d658b66a3490018d74f
G3ntl3m4n <urbanpeter08@outlook.hu>
2026-03-15 22:10:33 +0200

    Initializing nvim config

---
+++ lua/settings.lua
@@ -0,0 +1,40 @@
+-- ======== 
+-- Settings
+-- ========
+
+
+-- Setting line numbering and relative line numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+
+-- Setting tabs to be 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+
+-- Setting smart indent
+vim.opt.smartindent = true
+
+
+-- Turning of text wraping
+vim.opt.wrap = false
+
+
+-- Setting searching settings
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+
+-- Colors
+vim.opt.termguicolors = true
+
+
+vim.opt.scrolloff = 8
+
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "120"
