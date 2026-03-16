a5aae38b2e8f938a7fe20a578ca9aaf80a177439
G3ntl3m4n <urbanpeter08@outlook.hu>
2026-03-16 02:03:19 +0200

    Added lazy.nvim package manager, gruvbox color theme, and basic plugins: telescope, treesitter, neotree

--- init.lua
+++ init.lua
@@ -3,3 +3,23 @@
 -- ==========
 
 require("settings")
+require("keymaps")
+require("autocmds")
+require("lazy-setup")
+
+-- Setting color theme
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Treesitter
+require('nvim-treesitter').install { 'lua', 'bash', 'cpp', 'python', 'html', 'css', 'javascript', 'json', 'kitty', 'zsh', 'markdown', 'markdown_inline' }
+
+-- Neo-tree
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = 'Toggle Neotree' })
---
+++ lua/keymaps.lua
@@ -0,0 +1,12 @@
+-- =======
+-- Keymaps
+-- =======
+
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Built-in file explorer
+vim.keymap.set("n", "<leader>ne", ":Ex<CR>", { desc = "Built-In File Explorer" })
+
+
