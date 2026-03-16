a360556db74d77254df73855f0459b9e8a9418c2
G3ntl3m4n <urbanpeter08@outlook.hu>
2026-03-16 20:03:07 +0200

    Added nvim-dap and overseer

--- init.lua
+++ init.lua
@@ -45,3 +45,50 @@ vim.lsp.config('clangd', {
 })
 
 vim.lsp.enable('clangd')
+
+
+-- Nvim-dap
+
+local dap = require("dap")
+local dapui = require("dapui")
+
+-- tell dap to use codelldb or gdb
+dap.adapters.codelldb = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+    args = { "--port", "${port}" },
+  },
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/src/", "file")
+    end,
+    cwd = "${workspaceFolder}/src",
+    stopOnEntry = false,
+  },
+}
+
+-- open/close ui automatically
+dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+
+dapui.setup()
+
+vim.keymap.set("n", "<F5>",  dap.continue)
+vim.keymap.set("n", "<F10>", dap.step_over)
+vim.keymap.set("n", "<F11>", dap.step_into)
+vim.keymap.set("n", "<F12>", dap.step_out)
+vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+
+
+-- Overseer
+require("overseer").setup()
+vim.keymap.set("n", "<leader>r", ":OverseerRun<CR>")
+vim.keymap.set("n", "<leader>t", ":OverseerToggle<CR>")
